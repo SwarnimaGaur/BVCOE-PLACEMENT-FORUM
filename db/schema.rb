306adfc234c33f3216bc11a8b42941de67fb3b14
pai96 <paianu96@gmail.com>
2017-07-16 16:59:01 +0530

    random

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170715123535) do
+ActiveRecord::Schema.define(version: 20170716070842) do
+
+  create_table "experiences", force: :cascade do |t|
+    t.integer  "round_no"
+    t.string   "content"
+    t.string   "tips"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "experiences", ["user_id"], name: "index_experiences_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170715123535) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
