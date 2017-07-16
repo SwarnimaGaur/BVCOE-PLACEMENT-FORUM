509c180525dd702b349065685ba0639943c67cc9
pai96 <paianu96@gmail.com>
2017-07-16 14:56:57 +0530

    corrected experience

--- db/migrate/20170716070842_create_experiences.rb
+++ db/migrate/20170716070842_create_experiences.rb
@@ -1,9 +1,9 @@
 class CreateExperiences < ActiveRecord::Migration
   def change
     create_table :experiences do |t|
+      t.integer :round_no
       t.string :content
       t.string :tips
-      t.numeric :round_no
       t.references :user, index: true, foreign_key: true
 
       t.timestamps null: false
